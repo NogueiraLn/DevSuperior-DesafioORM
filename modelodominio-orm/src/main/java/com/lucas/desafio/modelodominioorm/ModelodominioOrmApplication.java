144352e5872fa450063e9426dd2e391da0bbd144
Nog <lucas.nogueiramartins@gmail.com>
2024-05-16 23:45:52 -0300

    Initial Commit

---
+++ modelodominio-orm/src/main/java/com/lucas/desafio/modelodominioorm/ModelodominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.desafio.modelodominioorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelodominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelodominioOrmApplication.class, args);
+	}
+
+}
---
+++ modelodominio-orm/src/test/java/com/lucas/desafio/modelodominioorm/ModelodominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.desafio.modelodominioorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelodominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
