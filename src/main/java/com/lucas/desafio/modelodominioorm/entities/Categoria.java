7511b5ed2c2af7cb01b590327a1a515fd4f2b346
Nog <lucas.nogueiramartins@gmail.com>
2024-05-17 00:09:33 -0300

    Add: Entity Categoria

---
+++ src/main/java/com/lucas/desafio/modelodominioorm/entities/Categoria.java
@@ -0,0 +1,41 @@
+package com.lucas.desafio.modelodominioorm.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String descricao;
+
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String descricao) {
+		this.id = id;
+		this.descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+
+}
